9ef54530bc770ed6ba48c9a33b23f5b69021464d
makrist2 <38978002+makrist2@users.noreply.github.com>
2021-08-01 14:20:11 +0300

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = 'bot token. Received at @botfather'
 
-dou = 'https://dou.ua/calendar/tags/Python/%D0%9E%D0%B4%D0%B5%D1%81%D1%81%D0%B0/'   # Odessa link
+rss_dou = 'https://dou.ua/calendar/feed/Python/%D0%9E%D0%B4%D0%B5%D1%81%D1%81%D0%B0/'   # Odessa link
 
 chat_id = 'group ID. Simply add bot to the chat and call him with /start@<botname>. Visible in the console'
 owner_id = 'user ID. Can be obtained by sending the /start command to the bot. Visible in the console'
