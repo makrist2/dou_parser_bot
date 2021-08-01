a440de7045f10dbab143a94cf97bc6a146d71b2d
makrist <namerki@gmail.com>
2021-08-01 15:14:21 +0300

    parse exception handler

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import urllib.error
+
 import telebot
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-import regex as rx
-import unicodedata
 import logging
 import time
 import threading
@@ -10,12 +10,16 @@ import datetime
 import config
 
 bot = telebot.TeleBot(config.BOT_TOKEN, parse_mode=None)
-req = Request(config.dou, headers=config.HEADERS)
-page = urlopen(req).read()  # dou page
-soup = BeautifulSoup(page, 'lxml')  # souping
-
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
+
+
+try:
+    req = Request(config.rss_dou, headers=config.HEADERS)
+    page = urlopen(req).read()  # dou page
+    soup = BeautifulSoup(page, 'xml')
+except Exception as he:
+    bot.send_message(config.owner_id, f'Something went wrong:\n\n{he}')
 
 
 def events_updater():
@@ -31,29 +35,29 @@ def events_updater():
         if len(kinda_db_lines) < 2 or time_delta.total_seconds() > 86400:                       # date check
             events_lst = []
             try:
-                events = soup.find_all('article', class_='b-postcard')
+                events = soup.findAll('item')
                 for event in events:
-                    title = rx.sub(r'\p{C}', '', event.h2.a.text)
-                    date = rx.sub(r'\p{C}', '', event.div.span.text)
-                    link = event.a.get('href')
+                    title = event.find('title').text
+                    date = title.split(',')[1]
+                    link = event.find('link').text
                     if title in events_lst:
                         continue
                     else:
-                        events_lst.append({'Title': unicodedata.normalize("NFKD", title),
-                                           'Date': unicodedata.normalize("NFKD", date),
+                        events_lst.append({'Title': title.split(',')[0],
+                                           'Date': date,
                                            'Link': link,
                                            })
                 if len(kinda_db_lines) < 2 or kinda_db_lines[1] != events_lst[0].get('Link'):   # link check
                     for event in events_lst:
                         bot.send_message(config.chat_id,
-                                         f'Название: {event.get("Title")}\n'
-                                         f'Дата: {event.get("Date")}\n'
-                                         f'Ссылка: {event.get("Link")}')
+                                         f'<b>Название</b>: {event.get("Title")}\n'
+                                         f'<b>Дата</b>: {event.get("Date")}\n'
+                                         f'<b>Ссылка</b>: {event.get("Link")}', parse_mode='HTML')
                         kinda_db_file.close()
                         kinda_db_file = open('kindadb.txt', 'w')
                         kinda_db_file.writelines(str(datetime.datetime.now()) + '\n' + events_lst[0].get('Link'))
             except Exception as e:
-                bot.send_message(config.owner_id, logger.error(e))
+                logger.error(e)
         kinda_db_file.close()
         time.sleep(5)
 
@@ -76,7 +80,6 @@ def send_welcome(message):
     if str(message.from_user.id) == config.owner_id:
         last_date = datetime.datetime.now()
         last_link = ''
-        bot.reply_to(message, "Howdy, how are you doing?")
         kinda_db = open('kindadb.txt', 'r+')                # initial data
         kinda_db.truncate(0)                                #
         kinda_db.write(str(last_date) + '\n' + last_link)   #
@@ -85,4 +88,4 @@ def send_welcome(message):
         background_worker(events_updater())                 # starting handler
 
 
-bot.polling()
+bot.polling(none_stop=True)
